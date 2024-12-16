4afb6f7498bb11c14adf24f23f7332b5e0f382c2
Gustavo Pereira <108029506+GustavoPereira-Dev@users.noreply.github.com>
2024-12-16 20:54:06 -0300

    Create ConversaoDados.java

---
+++ AOC/ConversaoDados.java
@@ -0,0 +1,74 @@
+public class ConversaoDados{
+  public double bitByte(int bits){
+          return bits / 8;
+      } 
+         
+      public double byteBit(int bytes){
+          return bytes * 8;
+      }    
+        
+      public double bitKibibit(int bits){
+          return bits / 1024;
+      }  
+        
+      public double kibibitBits(int kibibits){
+          return kibibits * 1024;
+      }  
+        
+      public double byteKilobytes(int bytes){
+          return bytes / 1024;
+      }
+        
+      public double kilobytesBytes(int kilobytes){
+          return kilobytes * 1024;
+      }   
+        
+      public double kilobytesMegabytes(int kilobytes){
+          return kilobytes / 1024;
+      }
+
+      public double megabytesKilobytes(int megabytes){
+          return megabytes * 1024;
+      }
+
+      public double kibibitMegabit(int kibibits){
+          return kibibits / 1000;
+      }  
+        
+      public double megabitsKibibits(int megabits){
+          return megabits * 1000;
+      }  
+
+      public double megabytesGigabytes(int megabytes){
+          return megabytes / 1024;
+      }
+
+      public double gigabytesMegabytes(int gigabytes){
+          return gigabytes * 1024;
+      }
+
+      public double megabitsGigabits(int megabits){
+          return megabits / 1000;
+      }  
+        
+      public double gigabitsMegabits(int gigabits){
+          return gigabits * 1000;
+      }  
+
+      public double gigabytesTerabytes(int gigabytes){
+          return gigabytes / 1024;
+      }
+
+      public double terabytesGigabytes(int terabytes){
+          return terabytes * 1024;
+      }
+
+      public double gigabitsTerabits(int gigabits){
+          return gigabits / 1000;
+      }  
+        
+      public double terabitsGigabits(int terabits){
+          return terabits * 1000;
+      }  
+  
+}
