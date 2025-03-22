5a94324e31f9e9f843e0296b478dd06b1ca41c46
GustavoPereira-Dev <gustavopereira8919@gmail.com>
2025-03-22 00:19:44 -0300

    feat(SOII): Desenvolvendo codigos de thread em C++ no Linux

---
+++ SO/II/thread.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <thread>
+
+using namespace std;
+
+// Uma função que sera executada como uma Thread
+void task1(std::string msg)
+{
+   std::cout << "A thread está falando: " << msg;
+}
+
+int main()
+{
+   //Invocando uma thread não bloqueante para ser executada em paralelo.
+   thread t1(task1, "Olá");
+
+   //Aqui você tem seu programa pesado, que ocorre em paralelo com a thread
+
+   // O método JOIN aguarda a finalização da Thread para liberar recursos juntos
+   t1.join();
+}
