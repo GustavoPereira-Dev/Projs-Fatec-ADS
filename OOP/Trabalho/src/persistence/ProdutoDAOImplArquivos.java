081e4a020a0e80d75f8b8a38a84cd5595bb9e482
GustavoPereira-Dev <gustavopereira8919@gmail.com>
2025-06-07 22:01:12 -0300

    feat(OOP): Começando a implementação do DAO das classes pela manipulação de arquivos CSV

---
+++ OOP/Trabalho/src/persistence/ProdutoDAOImplArquivos.java
@@ -0,0 +1,45 @@
+package persistence;
+
+import java.util.List;
+
+import model.Produto;
+
+public class ProdutoDAOImplArquivos implements ProdutoDAO {
+
+    @Override
+    public List<Produto> lerTodosContatos() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'lerTodosContatos'");
+    }
+
+    @Override
+    public void guardar(Produto produto) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'guardar'");
+    }
+
+    @Override
+    public boolean atualizar(Long id, Produto produto) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'atualizar'");
+    }
+
+    @Override
+    public boolean excluir(Long id) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'excluir'");
+    }
+
+    @Override
+    public Produto procurarPorId(Long id) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'procurarPorId'");
+    }
+
+    @Override
+    public List<Produto> pesquisarPorNome(String nome) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'pesquisarPorNome'");
+    }
+    
+}
