dffe115016c4bd8668870b9d8803f58a0ed65724
Gustavo Pereira <108029506+GustavoPereira-Dev@users.noreply.github.com>
2025-10-06 11:59:12 -0300

    fix(LabBD): Corrigindo os codigos dos arquivos SQL e a estrutura do projeto da Loja

--- LabBD/ProjetosSpring/SpringWebBasico/LojaSpring/Loja.sql
+++ LabBD/ProjetosSpring/SpringWebBasico/LojaSpring/Loja.sql
@@ -1,12 +1,71 @@
 CREATE DATABASE Loja;
+GO
 USE Loja;
 
--- Crie um CRUD Java Web com Spring com as operações de Inserir, Atualizar, Excluir, Selecionar Um,
+-- Crie um CRUD Java Web com Spring com as operaÃ§Ãµes de Inserir, Atualizar, Excluir, Selecionar Um,
 -- Selecionar Tudo e Selecionar para uma UDF, com base nas tabelas abaixo, retornar
 -- UDF (Nome do Cliente, Nome do Produto, Quantidade e Valor Total, Data de hoje)
 -- Tabelas iniciais:
--- Cliente (Código, nome)
--- Produto (Código, nome, valor)
+-- Cliente (CÃ³digo, nome)
+-- Produto (CÃ³digo, nome, valor)
+
+CREATE TABLE Cliente (
+    Codigo INT PRIMARY KEY,
+    Nome VARCHAR(100)
+);
+GO
+CREATE TABLE Produto (
+    Codigo INT PRIMARY KEY,
+    Nome VARCHAR(100),
+    Valor DECIMAL(10, 2)
+);
+GO
+-- SimulaÃ§Ã£o de compra
+CREATE TABLE Compra (
+    Codigo INT,
+    Codigo_Cliente INT,
+    Codigo_Produto INT,
+    Quantidade INT,
+    PRIMARY KEY (Codigo, Codigo_Cliente, Codigo_Produto),
+    FOREIGN KEY (Codigo_Cliente) REFERENCES Cliente(Codigo),
+    FOREIGN KEY (Codigo_Produto) REFERENCES Produto(Codigo)
+);
+
+INSERT INTO Cliente VALUES
+(1, 'Gustavo'),
+(2, 'Mariana'),
+(3, 'Felipe');
+GO
+INSERT INTO Produto VALUES
+(1, 'Notebook', 3500.00), (2, 'Mouse', 80.00), (3, 'Teclado', 120.00);
+GO
+INSERT INTO Compra VALUES
+(101, 1, 2, 2),
+(102, 2, 1, 1),
+(103, 3, 3, 3); 
+
+CREATE FUNCTION fn_dadoscompra()
+RETURNS @tabela TABLE(
+	Codigo INT,
+    Nome_Cliente VARCHAR(100),
+    Nome_Produto VARCHAR(100),
+    Quantidade INT,
+    Valor_Total DECIMAL(10,2),
+    Data_Hoje DATE
+)
+AS
+BEGIN
+    INSERT INTO @tabela (Codigo, Nome_Cliente, Nome_Produto, Quantidade, Valor_Total, Data_Hoje)
+        SELECT cp.Codigo AS Codigo, c.Nome AS Nome_Cliente, p.Nome AS Nome_Produto, cp.Quantidade, 
+               (cp.Quantidade * p.Valor) AS Valor_Total,
+               GETDATE() AS Data_Hoje
+        FROM Compra cp
+        JOIN Cliente c ON cp.Codigo_Cliente = c.Codigo
+        JOIN Produto p ON cp.Codigo_Produto = p.Codigo
+    RETURN
+END;
+
+SELECT * FROM fn_dadoscompra();
 
 -- Inserir Cliente
 CREATE PROCEDURE sp_inserir_cliente (
@@ -24,6 +83,8 @@ BEGIN
         SET @erro = ERROR_MESSAGE()
         IF(@erro LIKE '%PRIMARY KEY%')
             RAISERROR('Cliente ja existente!', 16, 1)
+		ELSE
+			RAISERROR(@erro, 16, 1)
     END CATCH
     SET @resultado = 'Cliente adicionado com sucesso';
 END;
@@ -65,15 +126,14 @@ END;
 CREATE PROCEDURE sp_inserir_produto (
     @codigo INT,
     @nome VARCHAR(100),
-    @Valor DECIMAL(10,2),
-    @qtd INT,
+    @valor DECIMAL(10,2),
     @resultado VARCHAR(100) OUTPUT
 )
 AS
 BEGIN
     DECLARE @erro VARCHAR(MAX)
     BEGIN TRY
-        INSERT INTO Produto VALUES (@codigo, @nome, @valor, @qtd);
+        INSERT INTO Produto VALUES (@codigo, @nome, @valor);
     END TRY
     BEGIN CATCH
         SET @erro = ERROR_MESSAGE()
@@ -90,12 +150,11 @@ CREATE PROCEDURE sp_atualizar_produto (
     @codigo INT,
     @nome VARCHAR(100),
     @valor DECIMAL(10,2),
-    @qtd INT,
     @resultado VARCHAR(100) OUTPUT
 )
 AS
 BEGIN
-    UPDATE Produto SET nome = @nome, valor_unitario = @valor, qtd_estoque = @qtd WHERE codigo = @codigo;
+    UPDATE Produto SET nome = @nome, valor = @valor WHERE codigo = @codigo;
     SET @resultado = 'Produto atualizado com sucesso';
 END;
 
--- LabBD/SQLServer/EstruturaSequencial/UDFs/UDF1.sql
+++ LabBD/SQLServer/EstruturaSequencial/UDFs/UDF1.sql
@@ -1,6 +1,6 @@
--- 1. Criar uma database, criar as tabelas abaixo, definindo o tipo de dados e a rela��o PK/FK e popular com alguma massa de dados de teste (Suficiente para testar UDFs)
--- Funcion�rio (C�digo, Nome, Sal�rio)
--- Dependendente (C�digo_Dep, C�digo_Funcion�rio, Nome_Dependente, Sal�rio_Dependente)
+-- 1. Criar uma database, criar as tabelas abaixo, definindo o tipo de dados e a relação PK/FK e popular com alguma massa de dados de teste (Suficiente para testar UDFs)
+-- Funcionário (Código, Nome, Salário)
+-- Dependendente (Código_Dep, Código_Funcionário, Nome_Dependente, Salário_Dependente)
 CREATE DATABASE EmpresaDB;
 GO
 USE EmpresaDB;
@@ -20,14 +20,14 @@ CREATE TABLE Dependente(
 	FOREIGN KEY (Codigo_Funcionario) REFERENCES Funcionario(Codigo)
 );
 
--- Funcion�rios
+-- Funcionários
 INSERT INTO Funcionario VALUES
 (1, 'Ana', 3000.00),
 (2, 'Bruno', 4500.00),
 (3, 'Carlos', 5000.00),
 (4, 'Daniela', 3200.00),
 (5, 'Eduardo', 2800.00);
-
+GO
 -- Dependentes
 INSERT INTO Dependente VALUES
 (101, 1, 'Lucas', 500.00),
@@ -36,10 +36,10 @@ INSERT INTO Dependente VALUES
 (104, 3, 'Sofia', 800.00),
 (105, 3, 'Tiago', 900.00),
 (106, 4, 'Marina', 400.00),
-(107, 5, 'Jo�o', 300.00);
+(107, 5, 'João', 300.00);
 
--- a) C�digo no Github ou Pastebin de uma Function que Retorne uma tabela:
--- (Nome_Funcion�rio, Nome_Dependente, Sal�rio_Funcion�rio, Sal�rio_Dependente)
+-- a) Código no Github ou Pastebin de uma Function que Retorne uma tabela:
+-- (Nome_Funcionário, Nome_Dependente, Salário_Funcionário, Salário_Dependente)
 CREATE FUNCTION fn_tabeladependente()
 RETURNS @tabela TABLE (
 Nome_Funcionario VARCHAR(100),
@@ -57,7 +57,7 @@ BEGIN
 END
 
 SELECT * FROM fn_tabeladependente();
--- b) C�digo no Github ou Pastebin de uma Fun��o Escalar que retorna a soma dos Sal�rios dos dependentes, mais a do funcion�rio.
+-- b) Código no Github ou Pastebin de uma Função Escalar que retorna a soma dos Salários dos dependentes, mais a do funcionário.
 CREATE FUNCTION fn_somasalarios(@CodigoFuncionario INT)
 RETURNS DECIMAL(10,2)
 AS
@@ -73,14 +73,14 @@ BEGIN
 END;
 
 -- 2. Fazer uma Function que retorne
--- a) a partir da tabela Produtos (codigo, nome, valor unit�rio e qtd estoque), quantos produtos est�o com estoque abaixo de um valor de entrada
+-- a) a partir da tabela Produtos (codigo, nome, valor unitário e qtd estoque), quantos produtos estão com estoque abaixo de um valor de entrada
 CREATE TABLE Produto (
     codigo INT PRIMARY KEY,
     nome VARCHAR(100),
     valor_unitario DECIMAL(10, 2),
     qtd_estoque INT
 );
-
+GO
 INSERT INTO Produto VALUES
 (1, 'Teclado', 120.00, 10),
 (2, 'Mouse', 80.00, 5),
@@ -93,7 +93,7 @@ INSERT INTO Produto VALUES
 (9, 'Pen Drive', 40.00, 12),
 (10, 'HD Externo', 300.00, 0);
 
-CREATE FUNCTION fn_estoqueabaixoentrada(@Entrada INT)
+CREATE FUNCTION fn_estoqueabaixoentrada(@entrada INT)
 RETURNS INT
 AS
 BEGIN
@@ -104,8 +104,8 @@ BEGIN
     RETURN @total;
 END;
 
--- b) Uma tabela com o c�digo, o nome e a quantidade dos produtos que est�o com o estoque abaixo de um valor de entrada
-CREATE FUNCTION fn_tabelaprodutos(@Entrada INT)
+-- b) Uma tabela com o código, o nome e a quantidade dos produtos que estão com o estoque abaixo de um valor de entrada
+CREATE FUNCTION fn_tabelaprodutos(@entrada INT)
 RETURNS @tabela TABLE (
     codigo INT,
     nome VARCHAR(100),
@@ -114,12 +114,12 @@ RETURNS @tabela TABLE (
 AS 
 BEGIN
     DECLARE @total INT;
-    SELECT @total = dbo.fn_estoqueabaixoentrada(@Entrada);
+    SELECT @total = dbo.fn_estoqueabaixoentrada(@entrada);
 
     INSERT INTO @tabela (codigo, nome, qtd_produtos)
     SELECT codigo, nome, @total
     FROM Produto
-    WHERE qtd_estoque < @Entrada;
+    WHERE qtd_estoque < @entrada;
 
     RETURN;
 END
@@ -129,21 +129,22 @@ END
 -- Tabelas iniciais:
 -- Cliente (Codigo, nome)
 -- Produto (Codigo, nome, valor)
-CREATE DATABASE Loja;
+CREATE DATABASE LojaBD;
+GO
 USE Loja;
-
+GO
 CREATE TABLE Cliente (
     Codigo INT PRIMARY KEY,
     Nome VARCHAR(100)
 );
-
+GO
 CREATE TABLE Produto (
     Codigo INT PRIMARY KEY,
     Nome VARCHAR(100),
     Valor DECIMAL(10, 2)
 );
-
--- Simula��o de compra
+GO
+-- Simulação de compra
 CREATE TABLE Compra (
     Codigo INT,
     Codigo_Cliente INT,
@@ -158,20 +159,18 @@ INSERT INTO Cliente VALUES
 (1, 'Gustavo'),
 (2, 'Mariana'),
 (3, 'Felipe');
-
+GO
 INSERT INTO Produto VALUES
 (1, 'Notebook', 3500.00), (2, 'Mouse', 80.00), (3, 'Teclado', 120.00);
-
-
+GO
 INSERT INTO Compra VALUES
 (101, 1, 2, 2),
-(102, 2, 4, 1), 
-(103, 3, 5, 3);
+(102, 2, 1, 1),
+(103, 3, 3, 3); 
 
--- Nome do Cliente, Nome do Produto, Quantidade e Valor Total, Data de hoje
+
 CREATE FUNCTION fn_dadoscompra()
 RETURNS @tabela TABLE(
-    Codigo INT,
     Nome_Cliente VARCHAR(100),
     Nome_Produto VARCHAR(100),
     Quantidade INT,
@@ -180,10 +179,205 @@ RETURNS @tabela TABLE(
 )
 AS
 BEGIN
-    INSERT INTO @tabela (Codigo, Nome_Cliente, Nome_Produto, Quantidade, Valor_Total, Data_Hoje)
-        SELECT c.Nome AS Nome_Cliente, p.Nome AS Nome_Produto, cp.Quantidade, (cp.Quantidade * p.Valor_Unitario) AS Valor_Total,
-        GETDATE() AS Data_Compra FROM Compra cp JOIN Cliente c ON cp.Codigo_Cliente = c.Codigo JOIN Produto p ON cp.Codigo_Produto = p.Codigo
+    INSERT INTO @tabela (Nome_Cliente, Nome_Produto, Quantidade, Valor_Total, Data_Hoje)
+        SELECT c.Nome AS Nome_Cliente, p.Nome AS Nome_Produto, cp.Quantidade, 
+               (cp.Quantidade * p.Valor) AS Valor_Total,
+               GETDATE() AS Data_Hoje
+        FROM Compra cp
+        JOIN Cliente c ON cp.Codigo_Cliente = c.Codigo
+        JOIN Produto p ON cp.Codigo_Produto = p.Codigo
     RETURN
 END;
 
-SELECT * FROM fn_dadosCompra();
+SELECT * FROM fn_dadoscompra();-- 1. Criar uma database, criar as tabelas abaixo, definindo o tipo de dados e a relação PK/FK e popular com alguma massa de dados de teste (Suficiente para testar UDFs)
+-- Funcionário (Código, Nome, Salário)
+-- Dependendente (Código_Dep, Código_Funcionário, Nome_Dependente, Salário_Dependente)
+CREATE DATABASE EmpresaDB;
+GO
+USE EmpresaDB;
+GO
+CREATE TABLE Funcionario(
+	Codigo INT PRIMARY KEY,
+	Nome varchar(100),
+	Salario decimal(7,1)
+);
+GO
+CREATE TABLE Dependente(
+	Codigo_Dep INT,
+	Codigo_Funcionario INT,
+	Nome_Dependente varchar(100),
+	Salario_Dependente decimal(7,1)
+	PRIMARY KEY (Codigo_Dep, Codigo_Funcionario),
+	FOREIGN KEY (Codigo_Funcionario) REFERENCES Funcionario(Codigo)
+);
+
+-- Funcionários
+INSERT INTO Funcionario VALUES
+(1, 'Ana', 3000.00),
+(2, 'Bruno', 4500.00),
+(3, 'Carlos', 5000.00),
+(4, 'Daniela', 3200.00),
+(5, 'Eduardo', 2800.00);
+GO
+-- Dependentes
+INSERT INTO Dependente VALUES
+(101, 1, 'Lucas', 500.00),
+(102, 1, 'Lara', 600.00),
+(103, 2, 'Pedro', 700.00),
+(104, 3, 'Sofia', 800.00),
+(105, 3, 'Tiago', 900.00),
+(106, 4, 'Marina', 400.00),
+(107, 5, 'João', 300.00);
+
+-- a) Código no Github ou Pastebin de uma Function que Retorne uma tabela:
+-- (Nome_Funcionário, Nome_Dependente, Salário_Funcionário, Salário_Dependente)
+CREATE FUNCTION fn_tabeladependente()
+RETURNS @tabela TABLE (
+Nome_Funcionario VARCHAR(100),
+Nome_Dependente VARCHAR(100),
+Salario_Funcionario decimal(7,1),
+Salario_Dependente decimal(7,1)
+)
+AS
+BEGIN
+	INSERT INTO @tabela (Nome_Funcionario, Nome_Dependente, Salario_Funcionario, Salario_Dependente) 
+		SELECT f.Nome AS Nome_Funcionario, d.Nome_Dependente, f.Salario AS Salario_Funcionario,
+        d.Salario_Dependente FROM Funcionario f JOIN Dependente d ON f.Codigo = d.Codigo_Funcionario
+
+	RETURN
+END
+
+SELECT * FROM fn_tabeladependente();
+-- b) Código no Github ou Pastebin de uma Função Escalar que retorna a soma dos Salários dos dependentes, mais a do funcionário.
+CREATE FUNCTION fn_somasalarios(@CodigoFuncionario INT)
+RETURNS DECIMAL(10,2)
+AS
+BEGIN
+    DECLARE @total DECIMAL(10,2);
+
+    SELECT @total = f.Salario + SUM(d.Salario_Dependente)
+    FROM Funcionario f
+    LEFT JOIN Dependente d ON f.Codigo = d.Codigo_Funcionario
+    WHERE f.Codigo = @CodigoFuncionario GROUP BY f.Salario;
+
+    RETURN @total;
+END;
+
+-- 2. Fazer uma Function que retorne
+-- a) a partir da tabela Produtos (codigo, nome, valor unitário e qtd estoque), quantos produtos estão com estoque abaixo de um valor de entrada
+CREATE TABLE Produto (
+    codigo INT PRIMARY KEY,
+    nome VARCHAR(100),
+    valor_unitario DECIMAL(10, 2),
+    qtd_estoque INT
+);
+GO
+INSERT INTO Produto VALUES
+(1, 'Teclado', 120.00, 10),
+(2, 'Mouse', 80.00, 5),
+(3, 'Monitor', 900.00, 2),
+(4, 'Notebook', 3500.00, 0),
+(5, 'Cabo HDMI', 35.00, 3),
+(6, 'Webcam', 250.00, 1),
+(7, 'Headset', 180.00, 4),
+(8, 'Impressora', 600.00, 6),
+(9, 'Pen Drive', 40.00, 12),
+(10, 'HD Externo', 300.00, 0);
+
+CREATE FUNCTION fn_estoqueabaixoentrada(@entrada INT)
+RETURNS INT
+AS
+BEGIN
+    DECLARE @total INT;
+
+    SELECT @total = SUM(codigo) FROM Produto WHERE qtd_estoque < @Entrada GROUP BY qtd_estoque;
+
+    RETURN @total;
+END;
+
+-- b) Uma tabela com o código, o nome e a quantidade dos produtos que estão com o estoque abaixo de um valor de entrada
+CREATE FUNCTION fn_tabelaprodutos(@entrada INT)
+RETURNS @tabela TABLE (
+    codigo INT,
+    nome VARCHAR(100),
+    qtd_produtos INT
+)
+AS 
+BEGIN
+    DECLARE @total INT;
+    SELECT @total = dbo.fn_estoqueabaixoentrada(@entrada);
+
+    INSERT INTO @tabela (codigo, nome, qtd_produtos)
+    SELECT codigo, nome, @total
+    FROM Produto
+    WHERE qtd_estoque < @entrada;
+
+    RETURN;
+END
+
+-- 3. Criar, uma UDF, que baseada nas tabelas abaixo, retorne
+-- Nome do Cliente, Nome do Produto, Quantidade e Valor Total, Data de hoje
+-- Tabelas iniciais:
+-- Cliente (Codigo, nome)
+-- Produto (Codigo, nome, valor)
+CREATE DATABASE LojaBD;
+GO
+USE LojaBD;
+GO
+CREATE TABLE Cliente (
+    Codigo INT PRIMARY KEY,
+    Nome VARCHAR(100)
+);
+GO
+CREATE TABLE Produto (
+    Codigo INT PRIMARY KEY,
+    Nome VARCHAR(100),
+    Valor DECIMAL(10, 2)
+);
+GO
+-- Simulação de compra
+CREATE TABLE Compra (
+    Codigo INT,
+    Codigo_Cliente INT,
+    Codigo_Produto INT,
+    Quantidade INT,
+    PRIMARY KEY (Codigo, Codigo_Cliente, Codigo_Produto),
+    FOREIGN KEY (Codigo_Cliente) REFERENCES Cliente(Codigo),
+    FOREIGN KEY (Codigo_Produto) REFERENCES Produto(Codigo)
+);
+
+INSERT INTO Cliente VALUES
+(1, 'Gustavo'),
+(2, 'Mariana'),
+(3, 'Felipe');
+GO
+INSERT INTO Produto VALUES
+(1, 'Notebook', 3500.00), (2, 'Mouse', 80.00), (3, 'Teclado', 120.00);
+GO
+INSERT INTO Compra VALUES
+(101, 1, 2, 2),
+(102, 2, 1, 1),
+(103, 3, 3, 3); 
+
+
+CREATE FUNCTION fn_dadoscompra()
+RETURNS @tabela TABLE(
+    Nome_Cliente VARCHAR(100),
+    Nome_Produto VARCHAR(100),
+    Quantidade INT,
+    Valor_Total DECIMAL(10,2),
+    Data_Hoje DATE
+)
+AS
+BEGIN
+    INSERT INTO @tabela (Nome_Cliente, Nome_Produto, Quantidade, Valor_Total, Data_Hoje)
+        SELECT c.Nome AS Nome_Cliente, p.Nome AS Nome_Produto, cp.Quantidade, 
+               (cp.Quantidade * p.Valor) AS Valor_Total,
+               GETDATE() AS Data_Hoje
+        FROM Compra cp
+        JOIN Cliente c ON cp.Codigo_Cliente = c.Codigo
+        JOIN Produto p ON cp.Codigo_Produto = p.Codigo
+    RETURN
+END;
+
+SELECT * FROM fn_dadoscompra();
