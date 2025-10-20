e60eae095f35d1fc15f4ecba0f3c698f301ae37e
GustavoPereira-Dev <gustavopereira8919@gmail.com>
2025-10-20 17:57:35 -0300

    feat(LabBD): Iniciando a atividade de Agencia e Contas Bancarias com Triggers, Functions e Procedures

---
+++ LabBD/ProjetosSpring/SpringData/AgenciaSpringData/src/main/java/br/edu/fateczl/AgenciaSpringData/AgenciaSpringDataApplication.java
@@ -0,0 +1,13 @@
+package br.edu.fateczl.AgenciaSpringData;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AgenciaSpringDataApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AgenciaSpringDataApplication.class, args);
+	}
+
+}
---
+++ LabBD/ProjetosSpring/SpringData/AgenciaSpringData/src/test/java/br/edu/fateczl/AgenciaSpringData/AgenciaSpringDataApplicationTests.java
@@ -0,0 +1,13 @@
+package br.edu.fateczl.AgenciaSpringData;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AgenciaSpringDataApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
